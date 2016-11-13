4637f45fdd7d061ad1bf8ff249b041e7a770fcde
Finaris <metorres@mit.edu>
2016-11-13 14:44:51 -0500

    Commented on file

--- Random Batch Files/ChristmasLights.bat
+++ Random Batch Files/ChristmasLights.bat
@@ -1,4 +1,6 @@
 @echo off
+REM I look back at this later and obviously see how I could probably loop to echo these lines, but it's still nice
+REM This was one of the first handful of scripts I made, so I have it here for nostalgia despite its simplicity/brute nature
 :x
 color 0a
 cls
